e70e69b8b789ebe14184724839a5ea691f035080
i85dev <i85dev@163.com>
2017-04-12 15:39:47 +0800

    add css js files and modify home

--- www/handlers.py
+++ www/handlers.py
@@ -9,9 +9,20 @@ from models import User, Comment, Blog, next_id
 @get('/')
 async def index(request):
     logging.info('index begin run')
-    users = await User.findAll()
-    logging.info('index run, request: %s, %s, users = %s' % (request.method, request.path, users)) 
+    summary = 'this is summary for the article'
+    blogs = [
+            Blog(id='1', name='Test Blog', summary=summary, created_at=time.time()-120),
+            Blog(id='2', name='Something New', summary=summary, created_at=time.time()-3600),
+            Blog(id='3', name='Learn Swift', summary=summary, created_at=time.time()-7200)
+            ]
     return {
-            '__template__': 'test.html',
-            'users': users
+            '__template__': 'blogs.html',
+            'blogs': blogs
             }
+
+#    users = await User.findAll()
+#    logging.info('index run, request: %s, %s, users = %s' % (request.method, request.path, users)) 
+#    return {
+#            '__template__': 'test.html',
+#            'users': users
+#            }
